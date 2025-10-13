d10c1a2b50d11ac6b15296b620469520521257c1
jrmarcco <jrmarcco@gmail.com>
2025-10-13 18:41:04 +0800

    feat: message.

--- api/go/message/v1/message.pb.validate.go
+++ api/go/message/v1/message.pb.validate.go
@@ -60,9 +60,11 @@ func (m *Message) validate(all bool) error {
 
 	var errors []error
 
-	// no validation rules for MessageId
+	// no validation rules for BizKey
 
 	// no validation rules for Cmd
+
+	// no validation rules for SerializeType
 
 	// no validation rules for Body
 
@@ -142,3 +144,112 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = MessageValidationError{}
+
+// Validate checks the field values on PushMessage with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *PushMessage) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on PushMessage with the rules defined in
+// the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in PushMessageMultiError, or
+// nil if none found.
+func (m *PushMessage) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *PushMessage) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for BizKey
+
+	// no validation rules for BizId
+
+	// no validation rules for ReceiverId
+
+	// no validation rules for SerializeType
+
+	// no validation rules for Body
+
+	if len(errors) > 0 {
+		return PushMessageMultiError(errors)
+	}
+
+	return nil
+}
+
+// PushMessageMultiError is an error wrapping multiple validation errors
+// returned by PushMessage.ValidateAll() if the designated constraints aren't met.
+type PushMessageMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m PushMessageMultiError) Error() string {
+	msgs := make([]string, 0, len(m))
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m PushMessageMultiError) AllErrors() []error { return m }
+
+// PushMessageValidationError is the validation error returned by
+// PushMessage.Validate if the designated constraints aren't met.
+type PushMessageValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e PushMessageValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e PushMessageValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e PushMessageValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e PushMessageValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e PushMessageValidationError) ErrorName() string { return "PushMessageValidationError" }
+
+// Error satisfies the builtin error interface
+func (e PushMessageValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sPushMessage.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = PushMessageValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = PushMessageValidationError{}
